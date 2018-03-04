f16560b92cf3b3d244f08208de71791a65ad7fd9
natyur <natalia.yurkovets@gmail.com>
2018-03-04 20:25:37 +0300

    chore(test): add tests

--- contracts/TestConfigurator.sol
+++ contracts/TestConfigurator.sol
@@ -2,56 +2,48 @@ pragma solidity ^0.4.18;
 
 import './ownership/Ownable.sol';
 
-contract UBCoinToken {
+contract BuyAndSellToken {
   function setSaleAgent(address newSaleAgent) public;
   function transferOwnership(address newOwner) public;
 }
 
 contract PreICO {
-  function setWallet(address newWallet) public;
   function setStart(uint newStart) public;
-  function setPeriod(uint newPerion) public;
+  function addMilestone(uint period, uint bonus) public;
   function setPrice(uint newPrice) public;
   function setMinInvestedLimit(uint newMinInvestedLimit) public;
+  function setSoftcap(uint newSoftcap) public;
   function setHardcap(uint newHardcap) public;
+  function setWallet(address newWallet) public;
   function setNextSaleAgent(address newICO) public;
   function setToken(address newToken) public;
   function transferOwnership(address newOwner) public;
 }
 
 contract ICO {
-  function addMilestone(uint period, uint bonus) public;
   function setStart(uint newStart) public;
+  function addMilestone(uint period, uint bonus) public;
   function setPrice(uint newPrice) public;
   function setMinInvestedLimit(uint newMinInvestedLimit) public;
   function setHardcap(uint newHardcap) public;
   function setWallet(address newWallet) public;
-  function setTeamTokensWallet(address newTeamTokensWallet) public;
-  function setBountyTokensWallet (address newBountyWallet) public;
-  function setReservedTokensWallet (address newReservedTokensWallet) public;
-  function setTeamTokensPercent(uint newTeamTokensPercent) public;
+  function setBountyTokensWallet(address newBountyWallet) public;
+  function setAdvisorsTokenWallet(address newAdvisorsTokenWallet) public;
+  function setDevelopersTokensWallet(address newDevelopersTokensWallet) public;
   function setBountyTokensPercent(uint newBountyTokensPercent) public;
-  function setReservedTokensPercent(uint newReservedTokensPercent) public;
-  function setToken(address newToken) public;
-  function transferOwnership(address newOwner) public;
-}
-
-contract FreezeTokensWallet {
-  function setStartLockPeriod(uint newStartLockPeriod) public;
-  function setPeriod(uint newPeriod) public;
-  function setDuration(uint newDuration) public;
+  function setAdvisorsTokenPercent(uint newAdvisorsTokenPercent) public;
+  function setDevelopersTokensPercent(uint newDevelopersTokensPercent) public;
   function setToken(address newToken) public;
   function transferOwnership(address newOwner) public;
 }
 
 contract TestConfigurator is Ownable {
-  UBCoinToken public token;
+  BuyAndSellToken public token;
   PreICO public preICO;
   ICO public ico;
-  FreezeTokensWallet public teamTokensWallet;
 
   function setToken(address _token) public onlyOwner {
-    token = UBCoinToken(_token);
+    token = BuyAndSellToken(_token);
   }
 
   function setPreICO(address _preICO) public onlyOwner {
@@ -62,44 +54,35 @@ contract TestConfigurator is Ownable {
     ico = ICO(_ico);
   }
 
-  function setTeamTokensWallet(address _teamTokensWallet) public onlyOwner {
-    teamTokensWallet = FreezeTokensWallet(_teamTokensWallet);
-  }
-
   function deploy() public onlyOwner {
     preICO.setWallet(0xa86780383E35De330918D8e4195D671140A60A74);
     preICO.setStart(1518393600);
-    preICO.setPeriod(15);
-    preICO.setPrice(33334000000000000000000);
+    preICO.setPrice(9000000000000000000000);
     preICO.setMinInvestedLimit(100000000000000000);
-    preICO.setHardcap(8500000000000000000000);
+    preICO.setHardcap(16000000000000000000000);
+    preICO.setSoftcap(500000000000000000000);
+    preICO.addMilestone(1, 40);
+    preICO.addMilestone(13, 30);
     preICO.setToken(token);
 
     token.setSaleAgent(preICO);
     preICO.setNextSaleAgent(ico);
 
     ico.setStart(1520640000);
-    ico.addMilestone(20, 40);
-    ico.addMilestone(20, 20);
-    ico.addMilestone(20, 0);
-    ico.setPrice(14286000000000000000000);
+    ico.addMilestone(7, 25);
+    ico.addMilestone(7, 15);
+    ico.addMilestone(14, 10);
+    ico.setPrice(4500000000000000000000);
     ico.setMinInvestedLimit(100000000000000000);
-    ico.setHardcap(96000000000000000000000);
+    ico.setHardcap(24000000000000000000000);
     ico.setWallet(0x98882D176234AEb736bbBDB173a8D24794A3b085);
     ico.setBountyTokensWallet(0x28732f6dc12606D529a020b9ac04C9d6f881D3c5);
-    ico.setReservedTokensWallet(0x28732f6dc12606D529a020b9ac04C9d6f881D3c5);
-    ico.setTeamTokensPercent(12);
-    ico.setBountyTokensPercent(4);
-    ico.setReservedTokensPercent(34);
+    ico.setAdvisorsTokensWallet(0x28732f6dc12606D529a020b9ac04C9d6f881D3c5);
+    ico.setDevelopersTokensWallet(0x28732f6dc12606D529a020b9ac04C9d6f881D3c5);
+    ico.setBountyTokensPercent(3);
+    ico.setAdvisorsTokensPercent(2);
+    ico.setDevelopersTokensPercent(20);
     ico.setToken(token);
-
-    ico.setTeamTokensWallet(teamTokensWallet);
-
-    teamTokensWallet.setStartLockPeriod(180);
-    teamTokensWallet.setPeriod(360);
-    teamTokensWallet.setDuration(90);
-    teamTokensWallet.setToken(token);
-    teamTokensWallet.transferOwnership(ico);
 
     token.transferOwnership(owner);
     preICO.transferOwnership(owner);
